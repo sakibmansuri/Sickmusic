2304c6098e8483b6117db76c8615bed124b9ecf4
Sakib Mansuri <sakibmansuri2305@gmail.com>
2025-12-08 21:42:48 +0530

    Expand music list with additional songs
    Added new songs to the music list.

--- music-list.js
+++ music-list.js
@@ -23,4 +23,40 @@ let allMusic = [
         img: "4",
         src: "Do Ya Wanna Taste It"
     },
+    {
+        name: "5 Years Time",
+        artist: "Noah and The Whale",
+        img: "5",
+        src: "5 Years Time"
+    },
+    {
+        name: "Can't Take My Eyes Off You",
+        artist: "Frank Sinatra",
+        img: "7",
+        src: "Can't Take My Eyes Off You"
+    },
+    {
+        name: "Carry On Wayward Son",
+        artist: "Kansas",
+        img: "6",
+        src: "Carry On Wayward Son"
+    },
+    {
+        name: "Let Down",
+        artist: "Radiohead",
+        img: "9",
+        src: "Let Down"
+    },
+    {
+        name: "My Way",
+        artist: "Frank Sinatra",
+        img: "8",
+        src: "My Way"
+    },
+    {
+        name: "Punkrocker",
+        artist: "Teddybears",
+        img: "5",
+        src: "Punkrocker"
+    },
 ]
