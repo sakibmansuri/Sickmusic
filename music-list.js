f84c4944c29b9447f4e1c06f4c6f929caf27f635
Sakib Mansuri <sakibmansuri2305@gmail.com>
2025-12-08 21:58:21 +0530

    Add comments to music entries
    Added comments to each music entry for clarity.

--- music-list.js
+++ music-list.js
@@ -1,58 +1,68 @@
 let allMusic = [
+    // 1
     {
         name: "I want it that way",
         artist: "Backstreet Boys",
         img: "1",
         src: "I want it that way"
     },
+    // 2
     {
         name: "Never gonna give you up",
         artist: "Rick Astley",
         img: "2",
         src: "Never Gonna Give You Up"
     },
+    // 3
     {
         name: "Back in Black",
         artist: "AC/DC",
         img: "3",
         src: "Back In Black"
     },
+    // 4
     {
         name: "Do ya wanna taste it?",
         artist: "Wig Wam",
         img: "4",
         src: "Do Ya Wanna Taste It"
     },
+    // 5
     {
         name: "5 Years Time",
         artist: "Noah and The Whale",
         img: "5",
         src: "5 Years Time"
     },
+    // 6
     {
         name: "Can't Take My Eyes Off You",
         artist: "Frank Sinatra",
         img: "7",
         src: "Can't Take My Eyes Off You"
     },
+    // 7
     {
         name: "Carry On Wayward Son",
         artist: "Kansas",
         img: "6",
         src: "Carry On Wayward Son"
     },
+    // 8
     {
         name: "Let Down",
         artist: "Radiohead",
         img: "9",
         src: "Let Down"
     },
+    // 9
     {
         name: "My Way",
         artist: "Frank Sinatra",
         img: "8",
         src: "My Way"
     },
+    // 10
     {
         name: "Punkrocker",
         artist: "Teddybears",
